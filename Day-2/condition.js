94e8e048d2744da49d8796d5d5661346aabf21bc
ehalt <torikussadikk@gmail.com>
2021-09-18 07:55:42 +0600

    again

---
+++ Day-2/condition.js
@@ -0,0 +1,88 @@
+// if 
+
+// let n1 = 21;
+// if (n1 > 20) {
+//     console.log('n1 is greater than 22');
+// }
+
+
+// else
+
+// let n2 = 23;
+// if (n2 > 24) {
+//     console.log('n2 is greater than 24')
+// }
+// else{
+//     console.log('n2 is less than 24')
+// }
+
+
+// else if 
+
+// let study = 9;
+// if (study > 7) {
+//     console.log('you are great student')
+// }
+// else if (study < 7) {
+//     console.log('you are good student')
+// }
+// else{
+//     console.log('I do not know about ya well')
+// }
+
+// switch
+
+// var day;
+
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = 'sunday';
+//         break;
+//     case 1:
+//         day = 'monday';
+//         break;
+//     case 2:
+//         day = 'Tuesday';
+//         break;
+//     case 3:
+//         day = 'Wednesday';
+//         break;
+//     case 4:
+//         day = 'Thursday';
+//         break;
+//     case 5:
+//         day = 'Friday';
+//         break;
+//     case 6:
+//         day = 'Saturday';
+// }
+// console.log(day)
+
+
+// ternary 
+
+// age = 23
+
+// var allow = age > 18 ? 'Dude! you can join this party ' : 'You better keep studying...'
+// console.log(allow);
+
+
+// logical operators 
+
+// a = 'tori'
+// b = 'kus'
+// if (a === 'tori' && b === 'kus') {
+//     console.log('I know you torikus')
+// }
+// else {
+//     console.log('something going wrong..')
+// }
+
+// name = 'tokyo'
+// if (name === 'tokyo' || name === 'TOkyo') {
+//     console.log('she is a crazy lover')
+// }
+// else{
+//     console.log('she dosent know Rio')
+// }
+
